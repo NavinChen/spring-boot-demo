406c0797507ef947bfa32ac199d204f288fc9bc0
chennw <chennw@lcjh.com>
2018-01-02 19:33:44 +0800

    save code

--- src/main/java/com/neo/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/neo/springbootdemo/SpringBootDemoApplication.java
@@ -2,11 +2,19 @@ package com.neo.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Hello Spring Boot";
 	}
 }
